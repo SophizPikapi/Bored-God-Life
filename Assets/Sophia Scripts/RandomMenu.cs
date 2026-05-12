f86cb68ae5e070cfeeaeb289a36dc3099aaccdd5
SophizPikapi <sophiasampana@gmail.com>
2026-05-12 20:42:30 +0100

    TEST

--- Assets/Sophia Scripts/RandomMenu.cs
+++ Assets/Sophia Scripts/RandomMenu.cs
@@ -9,25 +9,18 @@ public class RandomMenu : MonoBehaviour
     public int gameRand;
     public float timer = 10f;
     public bool gameStart = false;
-    public List<string> sceneList = new List<string>();
-    public int sceneNum;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
-        sceneNum = Random.Range(0, sceneList.Count);
-        SceneManager.LoadScene(sceneList[sceneNum]);
-        sceneList.RemoveAt(sceneNum);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (gameStart)
-        //{
-        //    SceneManager.LoadScene(sceneNum);
-        //    timer -= Time.deltaTime;
+        if (gameStart)
+        {
+            timer -= Time.deltaTime;
             //Dog Script
 
 
@@ -37,65 +30,63 @@ public class RandomMenu : MonoBehaviour
             //Stars Script
             if (timer <= 0 || refToGM.starWin)
             {
-                //gameRand = Random.Range(0, 3);
-                //timer = 10f;
-                //if (gameRand == 0)
-                //{
-                //    SceneManager.LoadScene("Dog");
-                //}
-                //if (gameRand == 1)
-                //{
-                //    SceneManager.LoadScene("Calendar");
-                //}
-                //if (gameRand == 2)
-                //{
-                //    SceneManager.LoadScene("Mouse");
-                //}
-                
+                gameRand = Random.Range(0, 3);
+                timer = 10f;
+                if (gameRand == 0)
+                {
+                    SceneManager.LoadScene("Dog");
+                }
+                if (gameRand == 1)
+                {
+                    SceneManager.LoadScene("Calendar");
+                }
+                if (gameRand == 2)
+                {
+                    SceneManager.LoadScene("Mouse");
+                }
             }
-            //else if (timer <= 0 && refToGM.starWin == false)
-            //{
-            //    refToGM.gameLives--;
-            //    gameRand = Random.Range(0, 3);
-            //    timer = 10f;
-            //    if (gameRand == 0)
-            //    {
-            //        SceneManager.LoadScene("Dog");
-            //    }
-            //    if (gameRand == 1)
-            //    {
-            //        SceneManager.LoadScene("Calendar");
-            //    }
-            //    if (gameRand == 2)
-            //    {
-            //        SceneManager.LoadScene("Mouse");
-            //    }
-            //}
-            
+            else if (timer <= 0 && refToGM.starWin == false)
+            {
+                refToGM.gameLives--;
+                gameRand = Random.Range(0, 3);
+                timer = 10f;
+                if (gameRand == 0)
+                {
+                    SceneManager.LoadScene("Dog");
+                }
+                if (gameRand == 1)
+                {
+                    SceneManager.LoadScene("Calendar");
+                }
+                if (gameRand == 2)
+                {
+                    SceneManager.LoadScene("Mouse");
+                }
+            }
 
             //Mouse Script
 
         }
     }
-    //private void OnMouseDown()
-    //{
-    //    gameRand = Random.Range(0, 4);
-    //    if (gameRand == 0)
-    //    {
-    //        SceneManager.LoadScene("Dog");
-    //    }
-    //    if (gameRand == 1)
-    //    {
-    //        SceneManager.LoadScene("Calendar");
-    //    }
-    //    if (gameRand == 2)
-    //    {
-    //        SceneManager.LoadScene("Stars");
-    //    }
-    //    if (gameRand == 3)
-    //    {
-    //        SceneManager.LoadScene("Mouse");
-    //    }
-    //    gameStart = true;
-    //}
-
+    private void OnMouseDown()
+    {
+        gameRand = Random.Range(0, 4);
+        if (gameRand == 0)
+        {
+            SceneManager.LoadScene("Dog");
+        }
+        if (gameRand == 1)
+        {
+            SceneManager.LoadScene("Calendar");
+        }
+        if (gameRand == 2)
+        {
+            SceneManager.LoadScene("Stars");
+        }
+        if (gameRand == 3)
+        {
+            SceneManager.LoadScene("Mouse");
+        }
+        gameStart = true;
+    }
+}
