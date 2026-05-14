35e8aca64cbfe4d5e091c34dca31f23427e6cb28
ZoinkerScoob <ziyahaci61@gmail.com>
2026-05-14 11:16:00 +0100

    MouseGame

--- Assets/Ziya Scripts/MouseMovement.cs
+++ Assets/Ziya Scripts/MouseMovement.cs
@@ -4,32 +4,62 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-  public bool Zapped = false;
+    public Rigidbody2D rb;
+    public bool Zapped = false;
+    public bool GameStart = false;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Zapped == false)
-        {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            transform.position = pos;
-        }
-        
 
-        
-        //Debug.Log("IsTriggered");
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float distance = Vector3.Distance(transform.position, pos);
+
+
+        if (GameStart == true)
+        {
+            if (Zapped == false)
+            {
+                if (distance > 0.5f)
+                {
+                    transform.up = pos - transform.position;
+                    rb.velocity = transform.up * speed * Time.deltaTime;
+                }
+                else
+                {
+                    rb.velocity = Vector3.zero;
+                }
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
+            //Debug.Log("IsTriggered");
+
+
+        }
+        else
+        {
+            if (distance < 0.5f)
+            {
+                GameStart = true;
+            }
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Electricity")){
+        if (collision.gameObject.CompareTag("Electricity"))
+        {
             Debug.Log("ZAP");
 
             Zapped = true;
